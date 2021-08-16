f8392bc7cc0b3653d660fdb4acce17ecd85bfd41
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-16 19:26:51 -0300

    store table properties setup

--- backend/src/app/database/migrations/1629146761586-CreateStore.ts
+++ backend/src/app/database/migrations/1629146761586-CreateStore.ts
@@ -3,7 +3,31 @@ import { MigrationInterface, QueryRunner, Table } from "typeorm";
 export class CreateStore1629146761586 implements MigrationInterface {
   private tablename = "store";
 
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: this.tablename,
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "store_name",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "store_owner",
+            type: "varchar",
+          },
+        ],
+      })
+    );
+  }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
 }
