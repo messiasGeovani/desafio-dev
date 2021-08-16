0c566a0deb298964f47db13d0d8a1a77b0d300a0
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-16 20:30:11 -0300

    validate fields method

--- backend/src/app/controllers/StoreController.ts
+++ backend/src/app/controllers/StoreController.ts
@@ -1,3 +1,15 @@
 import { Request, Response } from "express";
 
-export class StoreController {}
+    const store = await this.storeRepository.save(req.body);
+
+    return res.json(store);
+  }
+
+  private validateFields(store: IStoreDTO) {
+    if (!store.name || !store.owner) {
+      return false;
+    }
+
+    return true;
+  }
+}
