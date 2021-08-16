68ab0ab51f30e4a095ffe397f26b044d5184ffae
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-16 20:36:08 -0300

    store controller create method

--- backend/src/app/controllers/StoreController.ts
+++ backend/src/app/controllers/StoreController.ts
@@ -1,4 +1,19 @@
 import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { IStoreDTO } from "../dtos/IStoreDTO";
+import { StoreRepository } from "../repositories/StoreRepository";
+
+export class StoreController {
+  private storeRepository = getCustomRepository(StoreRepository);
+
+  public async create(req: Request, res: Response) {
+    const isValidStore = this.validateFields(req.body);
+
+    if (!isValidStore) {
+      return res.status(400).json({
+        message: "invalid fields",
+      });
+    }
 
     const store = await this.storeRepository.save(req.body);
 
