bf13b8856820368ecd350f769fb1458149b63d36
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-16 22:44:10 -0300

    transaction controller create

---
+++ backend/src/app/controllers/TransactionController.ts
@@ -0,0 +1,67 @@
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { ITransactionDTO } from "../dtos/ITransactionDTO";
+import { TransactionRepository } from "../repositories/TransactionRepository";
+
+export class TransactionController {
+  private transactionRepository = getCustomRepository(TransactionRepository);
+
+  public async create(req: Request, res: Response) {
+    const isValidStore = this.validateFields(req.body);
+
+    if (!isValidStore) {
+      return res.status(400).json({
+        message: "invalid fields",
+      });
+    }
+
+    await this.transactionRepository.save(req.body);
+
+    return res.json({
+      message: "transaction created",
+    });
+  }
+
+  public async index(req: Request, res: Response) {
+    const storeList = await this.transactionRepository.find();
+
+    return res.json(storeList);
+  }
+
+  public async find(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const transaction = await this.transactionRepository.findOne(id);
+
+    return res.json(transaction);
+  }
+
+  public async edit(req: Request, res: Response) {
+    const { id } = req.params;
+    const isValidFields = this.validateFields(req.body);
+
+    if (!isValidFields) {
+      return res.status(400).json({
+        message: "invalid fields",
+      });
+    }
+
+    await this.transactionRepository.update(id, req.body);
+
+    return res.json({
+      message: "transaction updated",
+    });
+  }
+
+  public async remove(req: Request, res: Response) {
+    const { id } = req.params;
+
+    await this.transactionRepository.delete(id);
+
+    return res.json({
+      message: "transaction deleted",
+    });
+  }
+
+  private validateFields(transaction: ITransactionDTO) {}
+}
