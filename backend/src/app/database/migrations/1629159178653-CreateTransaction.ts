cab1b60d4230978a889d622836568a32f4beff05
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-16 21:15:21 -0300

    create transaction migration

---
+++ backend/src/app/database/migrations/1629159178653-CreateTransaction.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateTransaction1629159178653 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
