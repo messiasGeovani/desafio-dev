09c58babf22083b5738a8425b1630cee103262af
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-16 21:03:11 -0300

    express app module initialization

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,41 +11,6 @@ createConnection()
     const app = new App().express;
     const port = process.env.PORT || 3000;
 
-    // create express app
-    const app = express();
-    app.use(bodyParser.json());
-
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next);
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
-
-            } else if (result !== null && result !== undefined) {
-                res.json(result);
-            }
-        });
-    });
-
-    // setup express app here
-    // ...
-
-    // start express server
-    app.listen(3000);
-
-    // insert new users for test
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Timber",
-        lastName: "Saw",
-        age: 27
-    }));
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Phantom",
-        lastName: "Assassin",
-        age: 24
-    }));
-
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
-
-}).catch(error => console.log(error));
+    app.listen(port, () => console.log(`server running on ${port}...`));
+  })
+  .catch((error) => console.log(error));
