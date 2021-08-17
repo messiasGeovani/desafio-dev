805441e4f0a679c2239bd36f371c6f8f32d81372
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-17 20:50:32 -0300

    app file with styled components configuration

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps /*, AppContext */ } from "next/app";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "../styles/GlobalStyles";
+import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
