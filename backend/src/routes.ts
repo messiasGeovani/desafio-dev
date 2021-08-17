794afaef4c08f3847861a57f820548b1cf076b47
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-16 22:52:45 -0300

    transaction controller attribute create

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,6 +4,7 @@ import { StoreController } from "./app/controllers/StoreController";
 export default class Routes {
   public routes;
   private storeController = new StoreController();
+  private transactionController = new TransactionController();
 
   constructor() {
     this.routes = Router();
