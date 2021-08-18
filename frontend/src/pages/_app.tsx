2375e1167174d002595a1f0d038de5560b9d4d52
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-18 15:09:45 -0300

    redux provider config

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,16 +1,18 @@
+import { Provider } from "react-redux";
 import type { AppProps /*, AppContext */ } from "next/app";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "../styles/GlobalStyles";
 import { theme } from "../styles/theme";
+import { store } from "../store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyles />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </Provider>
   );
 }
 
