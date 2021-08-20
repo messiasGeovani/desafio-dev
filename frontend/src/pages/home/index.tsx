2b59683c3b76d97163b37dd6759081188282d8cc
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-20 01:56:48 -0300

    auth validator on home page

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -2,17 +2,35 @@ import { SideBar } from "../../components/SideBar";
 import { TopBar } from "../../components/TopBar";
 import { Content } from "../../components/Content";
 
-import { Grid } from "./styles";
 import ContentProvider from "../../context/Content";
+import { AuthValidator } from "../../components/AuthValidator";
+
+import { Grid } from "./styles";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { getUser } from "../../store/reducers/userReducer";
+import { useEffect } from "react";
+import { setAuthToken } from "../../services/api";
 
 export default function Home() {
+  const dispatch = useAppDispatch();
+  const { user } = useAppSelector(getUser);
+
+  useEffect(() => {
+    if (user.token) {
+      setAuthToken(user.token);
+    }
+  }, [user.token]);
+
   return (
-    <ContentProvider>
-      <Grid>
-        <SideBar />
-        <TopBar />
-        <Content />
-      </Grid>
-    </ContentProvider>
+    <>
+      <AuthValidator />
+      <ContentProvider>
+        <Grid>
+          <SideBar />
+          <TopBar />
+          <Content />
+        </Grid>
+      </ContentProvider>
+    </>
   );
 }
