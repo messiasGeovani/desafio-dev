a9d7a8d696e08ddbf62665645323291a1f56a23f
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-19 23:08:03 -0300

    add user controller routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,17 +1,20 @@
 import { Router } from "express";
 import { StoreController } from "./app/controllers/StoreController";
 import { TransactionController } from "./app/controllers/TransactionController";
+import { UserController } from "./app/controllers/UserController";
 
 export default class Routes {
   public routes;
   private storeController = new StoreController();
   private transactionController = new TransactionController();
+  private userController = new UserController();
 
   constructor() {
     this.routes = Router();
 
     this.setStoreRoutes();
     this.setTransactionRoutes();
+    this.setUserRoutes();
   }
 
   private setStoreRoutes() {
@@ -39,6 +42,22 @@ export default class Routes {
 
     routes.post("/transactions", (req, res) => {
       this.transactionController.create(req, res);
+    });
+  }
+
+  private setUserRoutes() {
+    const { routes } = this;
+
+    routes.post("/users", (req, res) => {
+      this.userController.create(req, res);
+    });
+
+    routes.get("/users", (req, res) => {
+      this.userController.index(req, res);
+    });
+
+    routes.get("/users/:id", (req, res) => {
+      this.userController.find(req, res);
     });
   }
 }
