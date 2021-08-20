d0827e80390b47721cb07ec4fb3d66b2a9e291e5
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-20 01:58:39 -0300

    set token process config

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,11 +1,32 @@
 import axios, { AxiosResponse } from "axios";
 
-const api = axios.create({
+interface UserLogin {
+  email: string;
+  password: string;
+}
+
+let api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
+export function setAuthToken(token: string) {
+  api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
+
+export function login(userData: UserLogin): Promise<AxiosResponse> {
+  return api.post("/auth", userData);
+}
+
+export function signUp({ name, email, password }): Promise<AxiosResponse> {
+  return api.post("/users", { name, email, password });
+}
+
 export function sendTransactionsFile(file: File): Promise<AxiosResponse> {
-  console.log("here", process.env.API_URL);
   const formData = new FormData();
 
   formData.append("document", file);
