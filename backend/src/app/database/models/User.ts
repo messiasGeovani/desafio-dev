cf6dbbe461bb7a1780af49d2a556ed63198c2410
messiasGeovani <messias.geovani.lima@gmail.com>
2021-08-19 23:08:16 -0300

    user model change

--- backend/src/app/database/models/User.ts
+++ backend/src/app/database/models/User.ts
@@ -1,18 +1,29 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BeforeInsert,
+  BeforeUpdate,
+} from "typeorm";
+import * as bcrypt from "bcryptjs";
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column()
+  name: string;
 
-    @Column()
-    firstName: string;
+  @Column({ unique: true })
+  email: string;
 
-    @Column()
-    lastName: string;
+  @Column()
+  password: string;
 
-    @Column()
-    age: number;
-
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
 }
